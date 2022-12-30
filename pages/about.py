89945510897a1a331a9070f4917eb3650ae61894
silorepos <lorentzen@tuta.io>
2022-12-30 08:00:23 +0100

    Update layout

--- pages/about.py
+++ pages/about.py
@@ -11,14 +11,18 @@ layout = html.Div(
                             [
                                 html.H4("About this website", className="card-title"),
                                 html.P(
-                                    "This website aims to support the ongoing analysis and processing of the L1 data from the "
-                                    "GC-Net (see below). By providing an interactive and user-friendly visualization "
-                                    "environment, it enables researchers to explore processed data in new and interesting "
-                                    "ways. The website has two main sections: a data settings panel on the left side, "
-                                    "and a group of tabs for visualizing the data on the right side. The data settings panel "
-                                    "allows users to select a specific station, a year range, and to filter the data by key "
-                                    "variables. All of the interactive plots on the right side update automatically when the "
-                                    "data settings are changed.",
+                                    "GCNVis aims to support the ongoing analysis of the GC-Net data. "
+                                    ""
+                                    "We present an interactive visualization tool with "
+                                    "dynamic filter controls, linked views and historical data that ranges back to "
+                                    "the very first measurements. The mainpage consists of three sections: a data "
+                                    "settings panel, an interactive map and a group of tabs with relevant "
+                                    "visualizations. The data settings panel "
+                                    "allows users to select a specific station, a year range and filter the data "
+                                    "by "
+                                    "categories or customized variables of interest. The map and visualizations will then "
+                                    "update accordingly, providing a synergistic effect between interactive "
+                                    "exploration and comparative analysis. ",
                                     className="card-text",
                                 ),
                                 html.Br(),
@@ -35,12 +39,25 @@ layout = html.Div(
                                 html.Br(),
                                 html.H4("About the data", className="card-title"),
                                 html.P(
-                                    "This dataset consists of two main data levels: level 0 (L0) and level 1 (L1). L0 is the "
+                                    "The dataset has two main data levels: level 0 (L0) and level 1 (L1). L0 is the "
                                     "raw data from the dataloggers, historical processing codes, satellite transmissions, "
                                     "and Koni's personal data archive. L1 is the appended, calibrated, cleaned, and quality "
                                     "flagged data. It is provided in the newly described Non-Binary Environmental Data "
-                                    "Archive (NEAD) format, which is also CSV-compatible.",
+                                    "Archive (NEAD) format, which is also CSV-compatible. Links for the full processing scheme "
+                                    "and the latest L1 data are provided below. ",
                                     className="card-text",
+                                ),
+                                # html.Img(
+                                #     src="https://eng.geus.dk/Media/D/B/GC-Net%20AWS%202005%20after%20raising%20tower.JPG", height="500px", )
+                                dbc.CardImg(
+                                    src="https://eng.geus.dk/Media/D/B/GC-Net%20AWS%202005%20after%20raising%20tower.JPG",
+                                    className="align-items-center",
+                                    style={
+                                        "display": "block",
+                                        "margin-left": "auto",
+                                        "margin-right": "auto",
+                                        "width": "70%",
+                                    },
                                 ),
                             ]
                         ),
@@ -55,36 +72,27 @@ layout = html.Div(
                         dbc.Col(
                             dbc.Card(
                                 children=[
-                                    html.H4(
-                                        children="Access the data processing scheme here:",
-                                        className="text-left",
-                                    ),
                                     dbc.Button(
-                                        "Open GitHub",
+                                        "View processing scheme",
                                         href="https://github.com/GEUS-Glaciology-and-Climate/GC-Net-level-1-data-processing",
-                                        color="dark",
+                                        color="light",
                                     ),
                                 ],
-                                body=True,
                                 color="dark",
                                 outline=True,
                             ),
                             width=6,
+                            className="align-self-center",
                         ),
                         dbc.Col(
                             dbc.Card(
                                 children=[
-                                    html.H4(
-                                        children="Download the latest L1 data here:",
-                                        className="text-left",
-                                    ),
                                     dbc.Button(
-                                        "Open GitHub",
+                                        "Download data",
                                         href="https://github.com/GEUS-Glaciology-and-Climate/GC-Net-level-1-data-processing/tree/main/L1",
-                                        color="dark",
+                                        color="light",
                                     ),
                                 ],
-                                body=True,
                                 color="dark",
                                 outline=True,
                             ),
